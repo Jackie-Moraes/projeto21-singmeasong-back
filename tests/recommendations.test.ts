67ce331409320df84aa5d0f638c524b2da496148
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-07-29 21:34:16 -0300

    fix: removed unnecessary imports

--- tests/recommendations.test.ts
+++ tests/recommendations.test.ts
@@ -1,18 +1,12 @@
 import supertest from "supertest"
-import faker from "@faker-js/faker"
 
 import { prisma } from "../src/database.js"
 import app from "../src/app.js"
 import { recommendationsFactory } from "./factories/recommendationsFactory.js"
-import { Recommendation } from "@prisma/client"
 
 beforeEach(async () => {
     await prisma.$executeRaw`TRUNCATE TABLE recommendations`
 })
-
-// describe("POST /something", () => {
-//     it("should answer something when something happens", async () => {})
-// })
 
 describe("POST /recommendations", () => {
     it("should answer 201 when valid information is sent", async () => {

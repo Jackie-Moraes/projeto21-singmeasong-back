a9574f9fc205b6bc142620594928d49c93bca3dd
Jackie-Moraes <patrickandradepatrick@hotmail.com>
2022-07-29 23:01:13 -0300

    feat: test unit upvote

--- tests/unit/services.test.ts
+++ tests/unit/services.test.ts
@@ -1,7 +1,4 @@
-import supertest from "supertest"
-
 import { prisma } from "../../src/database.js"
-import app from "../../src/app.js"
 import { recommendationService } from "../../src/services/recommendationsService.js"
 import { recommendationRepository } from "../../src/repositories/recommendationRepository.js"
 import { servicesFactory } from "../factories/servicesFactory.js"
@@ -43,12 +40,39 @@ describe("insert", () => {
             return true
         })
 
-        const result = await recommendationService
-            .insert(recommendation)
-            .catch((err) => {
-                expect(err.message).toEqual(
-                    "Recommendations names must be unique"
-                )
-            })
+        await recommendationService.insert(recommendation).catch((err) => {
+            expect(err.message).toEqual("Recommendations names must be unique")
+        })
+    })
+})
+
+describe("upvote", () => {
+    it("should increase recommendation score when given valid id", async () => {
+        jest.spyOn(recommendationRepository, "find").mockImplementationOnce(
+            (): any => {
+                return true
+            }
+        )
+
+        jest.spyOn(
+            recommendationRepository,
+            "updateScore"
+        ).mockImplementationOnce((): any => {
+            return {}
+        })
+
+        const result = await recommendationService.upvote(1)
+        expect(result).toBeUndefined()
+    })
+
+    it("should throw error when given id is invalid", async () => {
+        jest.spyOn(recommendationRepository, "find").mockImplementationOnce(
+            (): any => {
+                return false
+            }
+        )
+        await recommendationService.upvote(1).catch((err) => {
+            expect(err.type).toEqual("not_found")
+        })
     })
 })
